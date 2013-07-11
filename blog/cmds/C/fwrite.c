63ab65fa1072b4c6b2813ed459445fc28e7b644a
Richard <richard.lee8681@gmail.com>
2013-07-11 15:20:54 +0800

    blog

--- blog/cmds/C/fwrite.c
+++ blog/cmds/C/fwrite.c
@@ -11,3 +11,51 @@ fseek对此函数有作用，但是fwrite[1]函数写到用户空间缓冲区，
 
 
 fwrite(&s, sizeof(s), 1, stream);  //将string s 中的 1 个sizeof(s)长度写入stream. 返回count数1
+
+
+
+/*using fread() and fwrite() to copy files.*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int
+main(int argc, char ** argv)
+{
+	FILE * filer, * filew;
+	int numr,numw;
+	char buffer[100];
+
+	if(argc!=3){
+		fprintf(stderr,"USAGE: binary-test inputfile outputfile.\n");
+		exit(1);
+	}
+
+	if((filer=fopen(argv[1],"rb"))==NULL){
+		fprintf(stderr, "open read file error.\n");
+		exit(1);
+	}
+
+	if((filew=fopen(argv[2],"wb"))==NULL){
+		fprintf(stderr,"open write file error.\n");
+		exit(1);
+	}
+	while(feof(filer)==0){
+	if((numr=fread(buffer,1,100,filer))!=100){
+		if(ferror(filer)!=0){
+		fprintf(stderr,"read file error.\n");
+		exit(1);
+		}
+		else if(feof(filer)!=0);
+	}
+	if((numw=fwrite(buffer,1,numr,filew))!=numr){
+		fprintf(stderr,"write file error.\n");
+		exit(1);
+	}
+	}
+
+	fclose(filer);
+	fclose(filew);
+
+	return 0;
+}
