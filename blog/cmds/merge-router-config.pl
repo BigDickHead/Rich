69732cf40507ecfe211423dbf0ac0756a353c96c
Richard <richard.lee8681@gmail.com>
2013-11-22 15:56:13 +0800

    cmds

---
+++ blog/cmds/merge-router-config.pl
@@ -0,0 +1,10 @@
+
+######################################################
+# Merge Cisco Router config  - Using SNMP
+# Hacked up by muts - muts@offensive-security.com
+#######################################################
+
+Usage : ./merge-copy-config.pl <router-ip> <tftp-serverip> <community> 
+
+Make sure a TFTP server is set up, prefferably running from /tmp ! 
+
